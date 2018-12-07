2f58e08861017e25b0bb28fe7f32cfdf66446734
李宁宁 <ning531267557@163.com>
2018-12-07 22:00:46 +0800

    add simplernn

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -25,4 +25,21 @@ for input_t in inputs:
 final_outputs = np.concatenate(successive_outputs, axis=0) #get the final_output with shape=(timesteps, output_features)
 
 # 5.1 SimpleRNN
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN, Embedding
+from keras import datasets
+from keras.preprocessing import sequence
 
+
+max_features = 10000 # 我们只考虑最常用的10k词汇
+maxlen = 100 # 每个评论我们只考虑100个单词
+
+(x_train, y_train), (x_test, y_test) = datasets.imdb.load_data(num_words=max_features)
+print(len(x_train), len(x_test))
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen) #长了就截断，短了就补0
+
+
+model = Sequential()
+model.add(Embedding())
